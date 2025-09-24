c37cd0ec77ba8c80a4d291a0accc67af006f4a83
msb <ben.sh@tohoku.ac.jp>
2025-09-25 05:45:51 +0800

    Bump version to 2.0.1 (code 2) for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.msb.bluecheese"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
